e46c4d2c7064dbb218de97acbec64ba707e95196
Sean Porter <portertech@gmail.com>
2012-06-04 19:59:04 -0700

    [ruby-1.9.x] prep for merge

--- test/test.sh
+++ test/test.sh
@@ -6,8 +6,6 @@ sudo apt-get install -y git-core build-essential libpam-dev
 
 git clone git://github.com/portertech/ruby-pam.git
 cd ruby-pam
-git fetch origin
-git checkout ruby-1.9.x
 
 ruby extconf.rb
 make
